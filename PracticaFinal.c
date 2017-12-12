2f2a26baf1a9c823093259c5e2902865980a98e5
Alejandro Martinez <alejandro.martinez.andres@alumnos.uva.es>
2017-12-12 20:30:53 +0100

    Replace PracticaFinal.c

--- PracticaFinal.c
+++ PracticaFinal.c
@@ -18,7 +18,7 @@
 
 
 #define TAMBUFF 4		//Tamaño buffer circular
-#define NUM_HCALC 6		//Numero de Chacales
+#define NUM_HCALC 6	//Numero de Chacales
 #define RUTAFICHD "./Datos.txt"
 #define RUTAFICHP "./Patron.txt"
 
@@ -49,72 +49,67 @@ int main()
   /*Inicializacion de semaforos*/
   sem_init(&hay_hueco_B,0,TAMBUFF);
   sem_init(&hay_dato_B,0,0);
+
   int i=0;
   pthread_t hiloCarga;  //Creacion del hilo
-	pthread_t hiloCalcula; //Creo los hilos consumidores
+	pthread_t hiloCalcula_i; //Creo los hilos consumidores
   pthread_create (&hiloCarga, NULL, (void *) &cargaDatos, (void *) NULL);
 
-	/*for(i=0;i<NUN_HCALC;i++){
+	for(i=0;i<0;i++){//sustituir el <0 por <NUM_HCALC
         int *arg;
-            printf("hjgsdjhfds");
         if ((arg = (int*)malloc(sizeof(int))) == NULL) {
           printf("No se puedo reservar memoria para arg.\n");
           exit(-1);
         }//lo copie de un ejemplo pero se instancia *arg dentro del for para que cada thread reciba una zona de memoria diferente como arguento y no haya problemas cuando vayan a leer el dato
-        *arg = i;*/
-        int *arg=0;
-       pthread_create (&hiloCalcula, 0 , (void *) &calcula_i, arg);
+        *arg = i;
+       pthread_create (&hiloCalcula_i, 0 , (void *) &calcula_i, arg);
 	//Aquí se va creando cada hilo, habria que mirar como enviar a cada uno el numero concreto de         hilo que es
 	//Nos hara mas tarde en el metodo calcula_i
-  //}
+  }
     /*El main acaba*/
-pthread_join(hiloCarga, NULL);
-  pthread_join(hiloCalcula, NULL); //Esto lo pongo pero Ni Puta Idea
-  printf("El hiloCarga ha termina'o...\n");
+  pthread_join(hiloCarga, NULL);
+  pthread_join(hiloCalcula_i, NULL); //Esto lo pongo pero Ni Puta Idea
+  printf("El hiloCarga ha termina'o...\n");fflush(stdout);
 
   exit(0);
 }
-
 /**
  * cargaDatos is used as the start routine for the threads used
  * it accepts a void pointer
 **/
 void cargaDatos ( )
 {
-
-  int i,j,k;
+  int i,j,k,cont=0;
   int aux[256];
-  int cont=0;
   FILE *fp;
   if((fp=fopen(RUTAFICHD,"r"))==NULL)
 	{
 		fprintf(stderr,"No se pudo abrir el fichero de datos %s\n", RUTAFICHD);
+    fflush(stdout);
 		exit(-1);
 	}
-
-	 sem_wait(&hay_hueco_B);
+	 sem_wait(&hay_hueco_B);//espera a que haya hueco en el buffer para poder escribir
 	for(k=0;k<1024;k++)	//He puesto el for que te comente para que no haya bucle infinito (en teoria XD)
 	{
     for(i = 0; i<256; i++)
     {
     fscanf(fp,"%d",&B[cont].vector[i]);
-  //  printf("El contenido de la linea %d es %d\n",B[cont].fila, B[cont].vector[i]);
+    //printf("El contenido de la linea %d es %d\n",B[cont].fila, B[cont].vector[i]);
     }
     B[cont].fila=k+1;
     cont = (cont+1)%TAMBUFF;
   }
-  sem_post(&hay_dato_B);
-
+  sem_post(&hay_dato_B);//señala que hay un dato el buffer
   for(i = 0; i<TAMBUFF; i++){
     for(j = 0; j < 256; j++){
         printf("El contenido de la linea %d es %d\n",B[i].fila, B[i].vector[j]);
+        fflush(stdout);
     }
     printf("\n");
   }
   fclose(fp);
   pthread_exit(0); /* exit */
 } /* print_message_function ( void *ptr ) */
-
 
 //Aqui comienza el metodo de los hilos calculadores
 void calcula_i(int numHil)
@@ -124,32 +119,25 @@ void calcula_i(int numHil)
   int filaRegistro_i;
   //semaforo
   //}Registro;
-
   //Registro Reg;//Creo esta estructura que en verdad no hace falta, pero al principio
 				//tenia otra cosa y dije bueno pues lo dejo, aunque se puede sacar de la struct
-
-  int i;
+  int i,aux;
   double resultado=0; // es double porque pow() devuelve double
-  int aux;
-    sem_wait(&hay_dato_B);
+    sem_wait(&hay_dato_B); //espera a que haya un dato en el buffer para poder vaciar la celda
     for(i = 0; i< 256; i++){
         vectorRegistro_i[i]=B[numHil].vector[i];
         printf("%d ",vectorRegistro_i[i]);
     }
   filaRegistro_i=B[numHil].fila;//Recojo en la estructura esta el contenido del buffer
   //Aqui recojo el dato de la posicion numHil, numHil debería ser lo que le pasamos al hilo que comente arriba
-    sem_post(&hay_hueco_B);
-    fflush(stdout);
-  printf("Se ha liberado la celda %d del buffer",filaRegistro_i);
-
-  fflush(stdout);
-
+    sem_post(&hay_hueco_B); // indica que hay un hueco en el buffer pues se ha vaciado una celda
+  printf("Se ha liberado la celda %d del buffer",filaRegistro_i);fflush(stdout);
   FILE *fp;
   if((fp=fopen(RUTAFICHP,"r"))==NULL)
 	{
 		fprintf(stderr,"No se pudo abrir el fichero de datos %s\n", RUTAFICHP);
 		exit(-1);
-	}//Abro el fichero patron
+	}//Abro el fichero patroooooooooooon
 	for (i=0;i<256;i++)//Si he entendido bien el enunciado hay que calcular la resta entre posiciones
 						//para cada uno de los 256 numeros que tenemos en el vector con los del patron
 						//elevarlo al cuadrado y sumarlos todos, despues hacemos la raiz
@@ -159,9 +147,8 @@ void calcula_i(int numHil)
 		resultado=resultado+pow((aux-vectorRegistro_i[i]),2);
 	}
 	resultado=sqrt(resultado);
-
-    R[numHil].fila=filaRegistro_i;//En el nuevo buffer añado la fila
-    R[numHil].D=resultado; //En el nuevo buffer añado la Distancia
+  R[numHil].fila=filaRegistro_i;//En el nuevo buffer añado la fila
+  R[numHil].D=resultado; //En el nuevo buffer añado la Distancia
 	//numHil sera lo que le enviamos que ya te comente arriba
 
 	// v(hay_dato_R[i])
